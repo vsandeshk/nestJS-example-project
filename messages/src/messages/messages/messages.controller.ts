2f3a2cd45dcf7a9ba2ddac67e10917255e46f5b2
vsandeshk <vsandeshk@gmail.com>
2025-02-02 14:53:41 +0100

    Added Get function Declaration in Messages Controller

--- messages/src/messages/messages/messages.controller.ts
+++ messages/src/messages/messages/messages.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages() {
+
+    }
+}
