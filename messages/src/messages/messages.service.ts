d1d6ee7336e6ea64af7f5aaea5aac62c51e7eb0d
vsandeshk <vsandeshk@gmail.com>
2025-02-02 23:22:26 +0100

    added service and its functions declaration for messages

---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MessagesService {
+    constructor(){}
+    
+    async findAll() {
+
+    }
+
+    async create(content: string) {
+
+    }
+
+    async findOne(id:string) {
+
+    }
+}
